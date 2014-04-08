9b7565324a646d07f689e080fc27db13e3b3abf5
Christian Schramm <christian-schramm83@gmx.de>
2014-04-08 09:59:10 +0200

    [Task] Latex-Vorlage

---
+++ clean.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+
+while [ 0 ] 
+do
+
+  #rm Symfony_Deployment.pdf
+
+  #pdflatex Symfony_Deployment.tex
+
+
+
+
+  rm *.out
+  rm *.log
+  rm *.aux
+  rm *.lof
+  rm *.idx
+  rm *.lot
+  rm *.dvi
+  rm *.toc
+  rm *.bst
+  rm *.blg
+  rm *.brf
+  rm *.*~
+
+  rm */*.out
+  rm */*.log
+  rm */*.aux
+  rm */*.backup
+
+  sleep 15
+done
